40ccb4e2007c8054bd11476f664ad6005707801d
James Newton <hello@jamesnewton.com>
2020-06-16 16:24:46 -0700

    include title/description meta in Head

--- app/javascript/helpers/Head.tsx
+++ app/javascript/helpers/Head.tsx
@@ -11,6 +11,8 @@ export default function Head({ meta: { title, description, url, type } }: Head) 
   return (
     <Helmet>
       <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:type" content={type} />

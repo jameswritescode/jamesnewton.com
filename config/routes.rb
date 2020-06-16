aa1fb01cc829468156a038439d88668d3bdee705
James Newton <hello@jamesnewton.com>
2020-06-16 16:56:27 -0700

    add resume redirect, fix status codes

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,19 @@
 Rails.application.routes.draw do
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
 
+  # NOTE: Support for redirecting legacy blog.jamesnewton.com
   constraints subdomain: 'blog' do
-    get '/', to: redirect('https://jamesnewton.com'), status: 302
+    get '/', to: redirect('https://jamesnewton.com', status: 302)
     get '/:any', to: redirect('https://jamesnewton.com/blog/%{any}')
   end
 
   post '/graphql', to: 'graphql#execute'
 
-  # get '/blog', to: 'application#home'
-  get '/blog' => redirect('/')
+  get '/blog', to: redirect('/')
   get '/blog/*path', to: 'application#home', as: :posts
 
-  # TODO
-  # get '/resume', to: 'application#resume'
+  # TODO: move / redesign resume
+  get '/resume', to: redirect('/', status: 302)
 
   root to: 'application#home'
 end

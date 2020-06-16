396ad093058b851eac0de02abdb4f37e12014cff
James Newton <hello@jamesnewton.com>
2020-06-16 03:17:39 -0700

    routes for old blog domain

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@
 Rails.application.routes.draw do
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
 
+  constraints subdomain: 'blog' do
+    get '/', to: redirect('https://jamesnewton.com'), status: 302
+    get '/:any', to: redirect('https://jamesnewton.com/blog/%{any}')
+  end
+
   post '/graphql', to: 'graphql#execute'
 
   # get '/blog', to: 'application#home'

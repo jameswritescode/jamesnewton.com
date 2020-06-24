e7fc3699593b6f55b3f39f13a4da7c4d303b9ab3
James Newton <hello@jamesnewton.com>
2020-06-23 19:05:06 -0700

    disable introspection in production

--- app/graphql/newton_schema.rb
+++ app/graphql/newton_schema.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class NewtonSchema < GraphQL::Schema
+  disable_introspection_entry_points if Rails.env.production?
+
   use GraphQL::Analysis::AST
   use GraphQL::Execution::Interpreter
 

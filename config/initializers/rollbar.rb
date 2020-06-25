f8c779a281c2dcd7fdf24a2eb093021cc1f10363
James Newton <hello@jamesnewton.com>
2020-06-25 14:36:58 -0700

    another pass at test workflow

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -4,7 +4,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = Rails.application.credentials[:rollbar][:access_token]
+  config.access_token = Rails.application.credentials[:rollbar][:access_token] unless Rails.env.test?
 
   # Here we'll disable in 'test':
   config.enabled = false unless Rails.env.production?
